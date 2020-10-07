ffb1caec44e9889af70a371d5ce4c919549244d1
Kacper Kucharski <kacpikuch1@icloud.com>
2020-10-07 14:22:32 +0200

    Initial Project setup with all the needed packages and a very simple routing to the Homepage

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='title'>Welcome To NGTI Workplace</div>
+);
+
+export default HomePage;
