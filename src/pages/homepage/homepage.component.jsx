c0ddd4233e0f803b9249748264353152d679f367
Patryk <0989317@hr.nl>
2020-10-11 23:21:59 +0200

    Images folder path
    images folder naar src folder verplaatst

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,8 +5,8 @@ import './homepage.styles.scss';
 const HomePage = () => (
     <body>
         <div>
-            <img src="/images/loginlogo.png" className="login-image"/>
-            <input type="image"  src="/images/google.jpg" className="google-image"/> 
+            <img src="../images/loginlogo.png" className="login-image"/>
+            <input type="image"  src="../images/google.jpg" className="google-image"/> 
         </div>
     </body>
 );
