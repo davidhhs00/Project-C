e864b4e1c74fe2d57154d48bd6082d84edb65d39
Patryk <0989317@hr.nl>
2020-10-12 00:01:59 +0200

    Images path (code)
    Path gefixt voor images

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import LoginLogo from "../../images/loginlogo.png"
+import GoogleSignIn from "../../images/google.jpg"
 import './homepage.styles.scss';
 
 const HomePage = () => (
     <body>
         <div>
-            <img src="../images/loginlogo.png" className="login-image"/>
-            <input type="image"  src="../images/google.jpg" className="google-image"/> 
+            <img src={LoginLogo} className="login-image"/>
+            <input type="image"  src={GoogleSignIn} className="google-image"/> 
         </div>
     </body>
 );
