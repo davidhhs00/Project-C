bb8c129d7992306f562d9042c9714149f1ac8117
Patryk <0989317@hr.nl>
2020-10-25 02:34:24 +0200

    Choose Group Page Styling
    Styling gemaakt voor Choose Group Page

--- src/pages/choosegrouppage/choosegroup.component.jsx
+++ src/pages/choosegrouppage/choosegroup.component.jsx
@@ -5,18 +5,23 @@ import './choosegroup.styles.scss';
 
 const ChooseGroupPage = () => (
      <div>
-        <button><p>Choose Group</p></button>
-        <p>Current Group:</p>
-        <img src={Logo}/>
-        <form>
-            <input type="text" id="colleague1"/><br/>
-            <input type="text" id="colleague2"/><br/>
-            <input type="text" id="colleague3"/><br/>
-            <input type="text" id="colleague4"/>
-        </form>
-        <button><p>Save Group</p></button>
-        <button><p>Back</p></button>
-        <button><p>Continue</p></button>
+        <div className="chgroup-button"><button>Choose Group</button></div>
+        <div><img src={Logo} className="ngti-logo"/></div>
+        <p className="choose-group">Current Group:</p>
+        <div className="inputvelden">
+            <form>
+                <input type="text" id="You"/><br/>
+                <input type="text" id="colleague1"/><br/>
+                <input type="text" id="colleague2"/><br/>
+                <input type="text" id="colleague3"/><br/>
+                <input type="text" id="colleague4"/>
+            </form>
+        </div><br/>
+        <button className="savegroup-button">Save Group</button>
+        <div>
+            <button className="back-button">Back</button>
+            <button className="continue-button">Continue</button>
+        </div>
     </div>
 );
 
