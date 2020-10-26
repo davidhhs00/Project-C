38bb3272feb6b04c9a2e9aaa735fa98b2eff735a
davidhr-dev <0993555@hr.nl>
2020-10-26 09:30:50 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-
+import './App.css';
 import Choose from './pages/welcomepage/choose.component';
 import Homepage from './pages/homepage/homepage.component';
-
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"; 
 
 class App extends React.Component {
   render() {
     return (
+      <Router>
+      <div>
       <Switch>
-        <Route exact path='/' component={Choose} />
-        <Route path='/' render={() => <Redirect to='/' />} />
+      <Route exact path='/' component={Homepage}/>
+      <Route path='/Choose' component={Choose}/>
       </Switch>
+      </div>
+      </Router>
     );
   }
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import LoginLogo from "../../images/loginlogo.png"
 import GoogleSignIn from "../../images/google.jpg"
 import './homepage.styles.scss';
+import {
+    Link
+  } from "react-router-dom"; 
 
 const HomePage = () => (
     <body>
         <div>
+        <Link to="/Choose">
             <img src={LoginLogo} className="login-image"/>
             <button className="google-button">Login met Google</button>
+        </Link>
         </div>
     </body>
 );
