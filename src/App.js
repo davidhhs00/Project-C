02af03550da8f9e16835e40be260cc64b1262258
TimoHR <72502786+TimoHR@users.noreply.github.com>
2020-11-02 00:14:17 +0100

    popups en routing
    pop ups gemaakt in yourreservations tab en de routing naar de yourreservations pagina gemaakt

--- src/App.js
+++ src/App.js
@@ -7,6 +7,34 @@ import { SubmitButton, InputField } from './Login.js'
 
 import './App.css';
 
+<<<<<<< Updated upstream
+=======
+import Login from './components/login/login.component';
+import Home from './components/Home/Home.component';
+import AllBookings from './components/all-booking/all-booking.component';
+import AllBookingsInfo from './components/all-booking-info/all-booking-info.component';
+import BookingInfo from './components/booking-info/booking-info.component';
+import ChooseGroup from './components/choose-group/choosegroup.component';
+import CustomButton from './components/custom-button/custom-button.component';
+import EndGroup from './components/end-group/end-group.component';
+import YourBookingsAdmin from './components/your-bookings-admin/your-bookings-admin.component';
+import SoloEndPage from './components/solo-end/solo-end.component';
+import YourBookingsSolo from './components/your-bookings-solo/your-bookings-solo.component';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+import HomePage from './components/login/login.component';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom"; 
+
+
+>>>>>>> Stashed changes
 class App extends React.Component {
   render() {
     return (
---
+++ src/components/home/Home.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from "../../assets/logo.png"
+import "./Home.styles.scss"
+
+
+const Choose = () => (
+        <div className = "main">
+            <button onClick={event =>  window.location.href='/'} className="button" id="log-outbtn">Logout</button>
+            <button id="adminbtn">Admin</button>
+            <img src={Logo} className="logo"/>
+            <h1 id="welcome-txt" className = "text">Welcome, User</h1>
+            <p id="booktxt" className="text">Book a workplace for:</p>
+            <button id="yourselfbtn" className = "button">Yourself</button>
+            <button onClick={event =>  window.location.href='/choosegroup'} id="groupbtn" className = "button">Group</button>
+            <p id="bookingstxt" className="text">See Bookings:</p>
+            <button onClick={event =>  window.location.href='/yourbookingssolo'} id="ybookingsbtn" className = "button">Your Bookings</button>
+            <button id="abookingsbtn" className = "button">All Bookings</button>
+        </div>
+);
+export default Choose;
