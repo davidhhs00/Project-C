80e8854f50ce0425a7bd9f3ff03bd1415b34ecbc
TimoHR <72502786+TimoHR@users.noreply.github.com>
2020-11-28 15:08:55 +0100

    added database functionalities
    now the table actually shows real data instead of placeholder data

--- src/components/your-bookings-solo/your-bookings-solo.component.jsx
+++ src/components/your-bookings-solo/your-bookings-solo.component.jsx
@@ -1,56 +1,63 @@
 import { app } from "firebase";
 import React from "react";
-
+import { firestore, auth } from '../../firebase/firebase.utils'
 import './your-bookings-solo.styles.scss';
 
+class yourBookingsSolo extends React.Component{
+  constructor(props) {
+    
+    super(props);
+   
+    this.state = {reservations : []}
+    }
 
-  const yourBookingsSolo = () => 
+  componentDidMount(){
+      console.log('hij werkt')
+      firestore.collection('reservations')
+        .get()
+        .then( snapshot => {
+          let reservations = [];
+          snapshot.forEach( doc => {
+            const data = doc.data()
+            reservations.push(data)
+          })
+          this.setState({ reservations: reservations })
+        })
+        .catch(error => console.log(error))
+  }
+  
+  render(){
+    return(
     <div>
       
       <table id='reservations'>  
+      <thead>
+          <tr>
+            <th>Who?</th>
+            <th>When?</th>
+            <th>Timeslot</th>
+            <th>Workplace</th>  
+          </tr>
+        </thead>
+        
+        <tbody> 
+        {this.state.reservations.map(data => {
+        return(
         <tr>
-          <th>datum</th>
-          <th>Werkplek</th>  
-          <th>Team</th>
-          <th>Met wie zit ik?</th>
-          <th>Tijdvak</th>
+          <td>{data.displayName}</td>
+          <td>{data.newStartDate}</td>
+          <td>{data.timeslot}</td>
+          <td>{data.workplace}</td>          
         </tr>
-
-        <tr>
-          <td>24-11-2020</td>
-          <td>Tafel 3</td>
-          <td>Team 2</td>
-          <td>David v/d Heuvel</td>
-          <td>Ochtend</td>
-        </tr>
-
-        <tr>
-          <td>24-11-2020</td>
-          <td>Tafel 3</td>
-          <td>Team 2</td>
-          <td>David v/d Heuvel</td>
-          <td>Ochtend</td>
-        </tr>
-
-        <tr>
-          <td>24-11-2020</td>
-          <td>Tafel 3</td>
-          <td>Team 2</td>
-          <td>David v/d Heuvel</td>
-          <td>Ochtend</td>
-        </tr>
-
-        <tr>
-          <td>24-11-2020</td>
-          <td>Tafel 3</td>
-          <td>Team 2</td>
-          <td>David v/d Heuvel</td>
-          <td>Ochtend</td>
-        </tr>
-
+        );
+        })}
+        </tbody>
       </table>
 
         <button onClick={event =>  window.location.href='/home'} id="backButton">Back</button>
 
     </div>
+    )
+  }
+}
 export default yourBookingsSolo;
