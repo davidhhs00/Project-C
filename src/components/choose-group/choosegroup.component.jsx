60468d162169f2f890c8e9e950a14e8a54448760
Patryk <0989317@hr.nl>
2020-11-29 21:10:48 +0100

    Lezen uit database
    Code leest nu users database uit en groups database uit.

--- src/components/choose-group/choosegroup.component.jsx
+++ src/components/choose-group/choosegroup.component.jsx
@@ -1,48 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import firebase from '../../firebase/firebase.utils'
 import { auth } from '../../firebase/firebase.utils';
 
 import Logo from "../../assets/logo.png"
 import './choosegroup.styles.scss';
 
-/*
-https://firebase.google.com/docs/auth/admin/manage-users#node.js_3
-https://firebase.google.com/docs/admin/setup#node.js
+// Functies om users te importen en laten zien
+// User List importeren
+function UserList(colleagueNumber) {
+  const [users, setUsers] = React.useState([])
 
-Voorbeeld code om alle users in een database te krijgen.
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore()
+      const data = await db.collection("users").get()
+      setUsers(data.docs.map(doc => doc.data()))
+    }
+    fetchData()
+  }, [])
 
+  return (
+    <select className="inputveld" id="colleaguenumber">
+      <option id="default">Select Colleague {colleagueNumber}</option>
+      {users.map(user =>(
+        <option key={user.displayName}>{user.displayName}</option>
+      ))}
+    </select>
+  );
+}
 
-admin.auth().getUsers([
-    { uid: 'uid1' },
-    { email: 'user2@example.com' },
-    { phoneNumber: '+15555550003' },
-    { providerId: 'google.com', providerUid: 'google_uid4' },
-  ])
-  .then(function(getUsersResult) {
-    console.log('Successfully fetched user data:');
-    getUsersResult.users.forEach((userRecord) => {
-      console.log(userRecord);
-    });
-
-    console.log('Unable to find users corresponding to these identifiers:');
-    getUsersResult.notFound.forEach((userIdentifier) => {
-      console.log(userIdentifier);
-    });
-  })
-  .catch(function(error) {
-    console.log('Error fetching user data:', error);
-  });
-*/
-
-const UserList = (colleagueNumber) => (
-  <select className="inputveld" id={colleagueNumber}>
-    <option id="default">Select Colleague {colleagueNumber}</option>
-    <option id="Patryk">Patryk</option>
-    <option id="David">David</option>
-  </select>
-);
-
+//Form maken voor gemaakte User Lists
 const GroupForm = ({currentUser}) => (
   <div className="inputvelden">
     <form>
@@ -55,25 +44,47 @@ const GroupForm = ({currentUser}) => (
       {UserList(4)}
     </form>
   </div>
-)
-
-//const SaveGroup = () => ();
-
-const SelectGroup = () => (
-  <select id="chgroup-button" className="chpbutton">
-    <option id="select">Select Group</option>
-    <option id="group1">Group 1</option>
-    <option id="group2">Group 2</option>
-  </select>
 );
 
+//Functies die zorgen voor het laten zien van groups en het opslaan van groups
+//Groups importeren
+function GroupList() {
+  const [groups, setGroups] = React.useState([])
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore()
+      const data = await db.collection("groups").get()
+      setGroups(data.docs.map(doc => doc.data()))
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <select id="chgroup-button" className="chpbutton">
+      <option id="default">Select Group</option>
+      {groups.map(group =>(
+        <option key={group.groupName}>{group.groupName}</option>
+      ))}
+    </select>
+  );
+}
+
+//Group exporteren
+function SaveGroup() {
+  return(
+    <button id="savegroup-button" className="chpbutton">Save Group</button>
+  );
+}
+
+//OUTPUT -> Functie voor export
 const ChooseGroup = (currentUser) => (
   <div className="align-center">
-    {SelectGroup()}
+    {GroupList()}
     <div><img src={Logo} className="ngti-logo"/></div>
     <p className="choose-group">Current Group:</p>
     {GroupForm(currentUser)}
-    <button id="savegroup-button" className="chpbutton">Save Group</button>
+    {SaveGroup()}
     <div className="align-center">
         <button id="back-button-group" className="chpbutton">Back</button>
         <button id="continue-button" className="chpbutton">Continue</button>
@@ -85,5 +96,4 @@ const mapStateToProps = ({user: {currentUser}}) => ({
     currentUser
 });
 
-//<button id="chgroup-button" className="chpbutton">Choose Group</button>
 export default connect(mapStateToProps)(ChooseGroup);
