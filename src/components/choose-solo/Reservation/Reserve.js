d849fa879476c2be63575a9aafffcfe4bbd83886
Michael-HR <michaellvb@live.nl>
2020-11-29 16:35:03 +0100

    Error handling

--- src/components/choose-solo/Reservation/Reserve.js
+++ src/components/choose-solo/Reservation/Reserve.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../../firebase/firebase.utils'
 import { firestore, auth} from '../../../firebase/firebase.utils'
+import SetRangeDates from './SetRangeDates'
 
 const sendReservation = async (props) => {
         const userRef = firestore.doc(`reservations/` + auth.currentUser.displayName)
@@ -11,17 +12,27 @@ const sendReservation = async (props) => {
             const {displayName, email} = auth.currentUser
             const {workplace, startDate, endDate, timeslot} = props
 
-            const newStartDate = startDate.format('MMMM Do YYYY')
-            const newEndDate = endDate.format('MMMM Do YYYY')
+            //Maybe add file for error handling?
+            const newStartDate = (startDate) ? startDate.format('YYYY-MM-DD') : ""
+            const newEndDate = (endDate) ? endDate.format('YYYY-MM-DD') : ""
 
+            let ReservedDates = ""
+            
+            if(newStartDate !== "" && newEndDate !== "" ) {
+                ReservedDates = SetRangeDates({newStartDate, newEndDate})
+            } else if(newStartDate === "" || newEndDate === ""){
+                ReservedDates = (newStartDate) ? newStartDate : newEndDate
+            } else {
+                return;
+            }
+        
             try {
                 await userRef.set({
                         displayName,
                         email,
                         workplace,
                         timeslot,
-                        newStartDate,
-                        newEndDate    
+                        ReservedDates  
                 })
             } catch (error) {
                 console.log('error sending user reservation', error.message);
