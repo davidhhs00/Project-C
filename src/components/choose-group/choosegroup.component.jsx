b2fe57e7d332f2f7309f4a7b4dc3ffb094546812
Patryk <0989317@hr.nl>
2020-12-05 17:43:14 +0100

    Big Update Choose Group Pagina
    1. colleagues worden nu op email en niet op naam opgeslagen in groepen( => omdat email altijd uniek is)
    2. Selecteren van groep selecteerd nu de bijbehorende members van de groep.
    3. Groep moet nu altijd een naam hebben.

--- src/components/choose-group/choosegroup.component.jsx
+++ src/components/choose-group/choosegroup.component.jsx
@@ -7,12 +7,32 @@ import firebase from '../../firebase/firebase.utils';
 import Logo from "../../assets/logo.png"
 import './choosegroup.styles.scss';
 
-
 //Group Form maken voor import en userlist importeren
 const GroupForm = ({currentUser}) => {
+  // Groups importeren
+  const [groups, setGroups] = React.useState([])
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore()
+      const data = await db.collection("groups").get()
+      setGroups(data.docs.map(doc => doc.data()))
+    }
+    fetchData()
+  }, [])
+
+  const handleChange = (e) => {
+    var eArray = e.split(",")
+    setColleague1(eArray[0])
+    setColleague2(eArray[1])
+    setColleague3(eArray[2])
+    setColleague4(eArray[3])
+  }
+  // Groups importeren
+
   // Group Form setup
   const [groupName, setgroupName] = useState("");
-  const [groupOwner] = useState(currentUser.displayName);
+  const [groupOwner] = useState(currentUser.email);
   const [colleague1, setColleague1] = useState("");
   const [colleague2, setColleague2] = useState("");
   const [colleague3, setColleague3] = useState("");
@@ -22,14 +42,19 @@ const GroupForm = ({currentUser}) => {
     e.preventDefault();
 
     const db = firebase.firestore()
-    db.collection("groups").add({
-      groupName: groupName,
-      groupOwner: groupOwner,
-      colleague1: colleague1,
-      colleague2: colleague2,
-      colleague3: colleague3,
-      colleague4: colleague4,
-    })
+
+    if (groupName === "") {
+      alert("Please Give your group a name");
+    } else {
+      db.collection("groups").add({
+        groupName: groupName,
+        groupOwner: groupOwner,
+        colleague1: colleague1,
+        colleague2: colleague2,
+        colleague3: colleague3,
+        colleague4: colleague4,
+      })
+    }
   };
   // Group Form setup
 
@@ -45,98 +70,86 @@ const GroupForm = ({currentUser}) => {
     fetchData()
   }, [])
   // Userlist Importeren
-  
-  return (
-  <div className="inputvelden">
-    <form onSubmit={handleSubmit}>
-
-      <select className="inputveld">
-        <option id="you">{currentUser.displayName}</option>
-      </select><br/>
-
-      {/*Collegues*/}
-      <select className="inputveld" id="colleaguenumber1" value={colleague1} onChange={(e) => setColleague1(e.target.value)}>
-        <option id="default" defaultValue>Select Colleague 1</option>
-        {users.map(user =>{
-        return user.displayName !== currentUser.displayName ?
-          <option key={user.email}>{user.displayName}</option>
-        :
-          null
-        })}
-      </select><br/>
-
-      <select className="inputveld" id="colleaguenumber2" value={colleague2} onChange={(e) => setColleague2(e.target.value)}>
-        <option id="default" defaultValue>Select Colleague 2</option>
-        {users.map(user =>{
-        return user.displayName !== currentUser.displayName ?
-          <option key={user.email}>{user.displayName}</option>
-        :
-          null
-        })}
-      </select><br/>
-
-      <select className="inputveld" id="colleaguenumber3" value={colleague3} onChange={(e) => setColleague3(e.target.value)}>
-        <option id="default" defaultValue>Select Colleague 3</option>
-        {users.map(user =>{
-        return user.displayName !== currentUser.displayName ?
-          <option key={user.email}>{user.displayName}</option>
-        :
-          null
-        })}
-      </select><br/>
-
-      <select className="inputveld" id="colleaguenumber4" value={colleague4} onChange={(e) => setColleague4(e.target.value)}>
-        <option id="default" defaultValue>Select Colleague 4</option>
-        {users.map(user =>{
-        return user.displayName !== currentUser.displayName ?
-          <option key={user.email}>{user.displayName}</option>
-        :
-          null
-        })}
-      </select><br/>
-      {/*Collegues*/}
-
-      <input className="groupname-input" placeholder="Group Name" value={groupName} onChange={(e) => setgroupName(e.target.value)}/><br />
-      
-      <button className="chpbutton" id="savegroup-button"  type="submit">Save Group</button>
-
-    </form>
-  </div>
-  )
-};
-
-//Groups importeren
-function GroupList({currentUser}) {
-  const [groups, setGroups] = React.useState([])
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const db = firebase.firestore()
-      const data = await db.collection("groups").get()
-      setGroups(data.docs.map(doc => doc.data()))
-    }
-    fetchData()
-  }, [])
 
   return (
-    <select id="chgroup-button" className="chpbutton" onChange={console.log(groups.groupName)}>
-      <option id="default">Select Group</option>
-      {groups.map(group =>{
-        return group.groupOwner === currentUser.displayName ?
-          <option key={group.groupName}>{group.groupName}</option>
+  <div>
+
+    <select id="chgroup-button" className="chpbutton" onChange={(e) => handleChange(e.target.value)}>
+      <option id="default" defaultValue value={["","","",""]}>Select Group</option>
+      {groups.map((group, index) =>{
+        return group.groupOwner === currentUser.email ?
+          <option key={group.groupName} value={[group.colleague1, group.colleague2, group.colleague3, group.colleague4]}>{group.groupName}</option>
         :
           null
       })}
     </select>
-  );
-}
+
+    <div><img src={Logo} className="ngti-logo" alt="ngti-logo"/></div>
+    <p className="choose-group">Current Group:</p>
+
+    <div className="inputvelden">
+      <form onSubmit={handleSubmit}>
+
+        <select className="inputveld">
+          <option id="you">{currentUser.displayName}</option>
+        </select><br/>
+
+        {/*Collegues*/}
+        <select className="inputveld" id="colleaguenumber1" value={colleague1} onChange={(e) => setColleague1(e.target.value)}>
+          <option id="default" defaultValue value="">Select Colleague 1</option>
+          {users.map(user =>{
+          return user.displayName !== currentUser.displayName ?
+            <option key={user.email} value={user.email}>{user.displayName}</option>
+          :
+            null
+          })}
+        </select><br/>
+
+        <select className="inputveld" id="colleaguenumber2" value={colleague2} onChange={(e) => setColleague2(e.target.value)}>
+          <option id="default" defaultValue value="">Select Colleague 2</option>
+          {users.map(user =>{
+          return user.displayName !== currentUser.displayName ?
+            <option key={user.email} value={user.email}>{user.displayName}</option>
+          :
+            null
+          })}
+        </select><br/>
+
+        <select className="inputveld" id="colleaguenumber3" value={colleague3} onChange={(e) => setColleague3(e.target.value)}>
+          <option id="default" defaultValue value="">Select Colleague 3</option>
+          {users.map(user =>{
+          return user.displayName !== currentUser.displayName ?
+            <option key={user.email} value={user.email}>{user.displayName}</option>
+          :
+            null
+          })}
+        </select><br/>
+
+        <select className="inputveld" id="colleaguenumber4" value={colleague4} onChange={(e) => setColleague4(e.target.value)}>
+          <option id="default" defaultValue value="">Select Colleague 4</option>
+          {users.map(user =>{
+          return user.displayName !== currentUser.displayName ?
+            <option key={user.email} value={user.email}>{user.displayName}</option>
+          :
+            null
+          })}
+        </select><br/>
+        {/*Collegues*/}
+
+        <input className="groupname-input" placeholder="Group Name" value={groupName} onChange={(e) => setgroupName(e.target.value)}/><br />
+        
+        <button className="chpbutton" id="savegroup-button"  type="submit">Save Group</button>
+
+      </form>
+    </div>
+
+  </div>
+  )
+};
 
 //OUTPUT -> Functie voor export
 const ChooseGroup = (currentUser) => (
   <div className="align-center">
-    {GroupList(currentUser)}
-    <div><img src={Logo} className="ngti-logo" alt="ngti-logo"/></div>
-    <p className="choose-group">Current Group:</p>
     {GroupForm(currentUser)}
     <button className="chpbutton" id="chback-button" onClick={event => window.location.href='/home'} type="button">Back</button>
     <button className="chpbutton" id="chcontinue-button" onClick={event => window.location.href='/choosesolo'} type="button">Continue</button>
