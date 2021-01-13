7f3daaa7966635aa4789af137a5cbb6148c4775b
TimoHR <72502786+TimoHR@users.noreply.github.com>
2021-01-13 17:12:35 +0100

    comments
    code gecommend voor duidelijkheid

--- src/components/your-bookings-solo/your-bookings-solo.component.jsx
+++ src/components/your-bookings-solo/your-bookings-solo.component.jsx
@@ -7,31 +7,32 @@ import './your-bookings-solo.styles.scss';
 
 
 class yourBookingsSolo extends React.Component{
+  //constructor die de props en de reservations en documentid arrays initialized
   constructor(props) {
     
     super(props);
 
     this.state = {
       reservations : [],
+      documentid : [],
       }
     }
   
+  //Deze functie haalt reservatie data en de documentID uit de firebase
   componentDidMount(){
-      //console.log('hij mount je weet toch')
-
-      //Alle reservaties uit de database halen
+      //Reservatie data gaat naar het reservations array
+      //documentid gaat naar het documentid array
       firestore.collection('reservations')
         .get()
         .then( snapshot => {
           let reservations = [];
           let documentid = [];
+          //onderstaande lines pushen voor elke reservatie de reservatie data (data) naar het reservations array en het documentid (docID) naar het documentid array
           snapshot.forEach( doc => {
             const data = doc.data()
             const docID = doc.id
             reservations.push(data)
             documentid.push(docID)
-            //console.log(documentid)
-            //console.log(data.firebaseDates)
           })
           this.setState({ reservations: reservations })
           this.setState({ documentid: documentid })
@@ -39,36 +40,48 @@ class yourBookingsSolo extends React.Component{
         .catch(error => console.log(error))
   }
   
-  //Verwijdert de correcte reservering uit de database.
+  //Verwijdert de correcte reservering uit de database. en refresht de pagina na 500ms
+  //i == het nummer van jouw reservatie in de tabel
   removeToCollection(i) {
+    // fetched het documentID door het nummer van de reservatie in de tabel te pakken en die id uit het documentid array te pakken
     let key = this.state.documentid[i]
-    //console.log(key)
+    //stuurt de command naar firebase om de reservatie te verwijderen, naar aanleiding van de key die hiervoor gefetched is
     firebase.firestore().collection("reservations").doc(key).delete()
+    //reload de pagina na 500ms, dit moet met een delay omdat anders de firebase geen command krijgt om de reservatie te verwijderen. 500ms is gekozen omdat het niet te lang is en wel goed werkt
     setTimeout(function(){window.location.reload(true);}, 500)
   }
 
   // Bijbehorende functie van CheckEmailandAdmin()
   // Checkt of je email hetzelfde is en of je admin bent
+  // x == data.email == de email van de gebruiker die in de reservatie staat 
+  //y == this.props.currentUser.email == de email van de gebruiker die op het moment ingelogd is
+  //z == i == het nummer van jouw reservatie in de tabel
+  //a == this.props.currentUser.admin == admin check
   isAuth(x, y, z, a){
+    //als allebei de emails gelijk zijn dan wordt de reservatie verwijderd
     if (x === y){
       this.removeToCollection(z)
     }
+    //als de gebruiker die nu is ingelogd een admin is dan wordt de reservatie verwijderd
     if (a){
       this.removeToCollection(z)
-      //console.log('admin detected')
     }
+    //als geen van beide waar is en de gebruiker kan toch op de delete knop klikken dan krijgt hij een alert. deze situatie kan in principe niet voorkomen.
     else if(x !== y && !a) {
       alert('dit kan niet')
     }
   }
 
-  //Schrijft de datums van de reservaties uit.
+  //zorgt dat er een komma en een spatie tussen de reservaties komt en dat de workplace wordt weergeven.
+  //x = data.firebaseDates
   Dates(x){
     var text = ""
+    //for loop die door alle dates in de data.firebaseDates heen loopt en de komma en spatie plaatst
       for(let i = 0; i < x.length; i++){
         if(i < x.length-1){
           text += x[i].Date + " Workplace: " + x[i].Workplace + ", "
         }
+        //bij de laatste date in de data.firebaseDates plaatst hij geen komma en spatie aan het einde
         else{
           text += x[i].Date + " Workplace: " + x[i].Workplace
         }
@@ -76,19 +89,21 @@ class yourBookingsSolo extends React.Component{
       return(text)
   }
 
-  //Laat de deletebutton zien bij de correcte reserveringen zien als 1 van de 2 waar is:
-  // 1 -> Je bent admin -> Laat bij alle reserveringen deletebutton zien.
-  // 2 -> Je bent de eigenaar van de reservatie -> laat de door jouw gemaakte reservaties zien.
+
   CheckEmailandAdmin() {
     return (
       this.state.reservations.map((data,i) =>{
         return data.email === this.props.currentUserCheck.email || this.props.currentUserCheck.admin === true?
+          //Laat de delete button zien bij de correcte reserveringen als 1 van de 3 waar is:
+          // 1 -> Je bent admin -> Laat bij alle reserveringen delete button zien.
+          // 2 -> Je bent de eigenaar van de reservatie -> laat bij de door jouw gemaakte reservaties de delete knop zien.
           <tr key={data.groupName ? [data.groupName,data.email,data.colleague1,data.colleague2,data.colleague3,data.colleague4] : data.email}>
             <td>{data.displayName}</td>
             <td>{this.Dates(data.firebaseDates)}</td>         
             <td> <button onClick={event => this.isAuth(data.email, this.props.currentUser.email, i, this.props.currentUser.admin)} id="deleteButton">Delete</button>  </td>
           </tr>
          :
+         // 3 -> je bent geen van beide -> laat geen delete knop zien. wel iedereen zijn reservaties, inclusief groepsreservaties
          <tr key={data.groupName ? [data.groupName,data.email,data.colleague1,data.colleague2,data.colleague3,data.colleague4] : data.email}>
           <td>{data.displayName}</td>
           <td>{this.Dates(data.firebaseDates)}</td>         
@@ -98,7 +113,7 @@ class yourBookingsSolo extends React.Component{
     )
   }
 
-  // HTML
+  // HTML rendert de tabel en voert de CheckEmailandAdmin functie uit, rendert ook de terug knop
   render(){
     return(
     <div>
@@ -124,11 +139,9 @@ class yourBookingsSolo extends React.Component{
   }
 }
 
-//Wordt gebruikt om de ingelogde persoon te krijgen
+//Wordt gebruikt om de informatie over de ingelogde persoon te krijgen
 const mapStateToProps = ({ user: { currentUser } }) => ({
   currentUser,
 });
 
 export default connect(mapStateToProps)(yourBookingsSolo);
-
-//<button onClick={event => this.isAuth(data.displayName, this.props.currentUser.displayName, i, this.props.currentUser.admin)} id="deleteButton">Delete</button>  delete `button 
