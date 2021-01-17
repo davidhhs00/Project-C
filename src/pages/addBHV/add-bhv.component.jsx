10c638cf85223c09da54b24ad6a5a2be91dd1008
Michael-HR <72655822+Michael-HR@users.noreply.github.com>
2021-01-17 21:26:04 +0100

    last changes

--- src/pages/addBHV/add-bhv.component.jsx
+++ src/pages/addBHV/add-bhv.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { getAllUsers, createNewDocument } from "../../firebase/firebase.utils";
 import "./addbhv.styles.scss";
 
+const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'] 
+
 export default class AddBhv extends React.Component {
   constructor(props) {
     super(props);
@@ -9,6 +11,8 @@ export default class AddBhv extends React.Component {
       people: [],
       personID: "",
       title: "BHV",
+      days: [],
+      amountDays: null
     };
     this.addBHV = this.addBHV.bind(this);
   }
@@ -23,14 +27,33 @@ export default class AddBhv extends React.Component {
 
   addBHV(e) {
     e.preventDefault();
-    createNewDocument(this.state.personID, this.state.title);
+    createNewDocument(this.state.personID, this.state.title, this.state.days);
+  }
+
+  amountOfDays = (e) => {
+    if(e.target.value > 0 && e.target.value <6){
+      this.setState({amountDays : e.target.value})
+    } else if(e.target.value == 0 || e.target.value > 5){
+      this.setState({amountDays : null})
+    }
+  }
+  setDays = (e, i) => {
+    e.persist()
+    this.setState(prevState => {
+      const {days} = prevState;
+        if(!days.includes(e.target.value)){
+           days.push(e.target.value);
+        }
+        return days
+      }
+    )
   }
 
   render() {
     return (
       <div className="bhv-page">
         <form>
-          <label htmlFor="people">Appoint new BHV: </label>
+          <label htmlFor="people">Appoint new BHV:</label>
           <br />
           <select className="input-btn"
             onChange={(e) => this.setState({ personID: e.target.value })}
@@ -45,6 +68,23 @@ export default class AddBhv extends React.Component {
               </option>
             ))}
           </select>
+          <br />
+          <label>Enter amount of days: </label>
+          <input className="input-btn" type='number' onChange={this.amountOfDays}></input>
+          <br />
+          <label>Select which days of the week:</label>
+          {(this.state.amountDays ? weekdays : []).map((key, i) => (
+              (i < this.state.amountDays ? <div key={i}>
+                    <select className="input-btn" type="select"key={i} onChange={this.setDays}>
+                      <option key="default" value="">Select an option</option>
+                      <option value="Monday">Monday</option>
+                      <option value="Tuesday">Tuesday</option>
+                      <option value="Wednesday">Wednesday</option>
+                      <option value="Thursday">Thursday</option>
+                      <option value="Friday">Friday</option>
+                    </select>
+                  </div> : "")
+          ))}
           <br />
           <button className="submit-btn" onClick={this.addBHV} type="submit">
             Add new BHV
